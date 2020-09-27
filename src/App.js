7160832fc275dfddba83e0fb35a58f0f6f3c568a
Sisu <66745372+nicoleta-denisa@users.noreply.github.com>
2020-09-27 21:22:44 +0300

    Homework 1 React

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState} from 'react';
+
+import './colors.css';
+
+const colors = ["#bada55", "#ff0000", "#a38181"];
 
 function App() {
+  const [color, setColor] = useState(colors[0]);
+
+  function handleClick() { 
+    function random() {
+      return parseInt(Math.floor(Math.random() * colors.length));
+    }
+    setColor (colors[random()]);
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Changing color scheme</h1>
+      <br></br>
+      <span style={ {backgroundColor: color}}  className="content">Currently Selected Color { color } </span>
+      <button onClick={ () => handleClick() } className="button">Change Color</button>    
     </div>
-  );
-}
+  )
+} 
 
 export default App;
